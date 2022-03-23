59d587f59c057dd368835996a034b74a5fefdcab
Andor Czafik <andor@czafik.hu>
2022-03-23 11:59:05 +0100

    Create info.php
    Source code for the default info script https://czandor.hu/fitbit/prog/info.php (BTC price)

---
+++ info.php
@@ -0,0 +1,36 @@
+<?php
+
+$json = file_get_contents('php://input');
+$obj = json_decode($json);
+$response=new StdClass();
+if($obj){
+    $uniqueid=isset($obj->uniqueid)?$obj->uniqueid:"uniqueid";
+    $modelId=isset($obj->modelId)?$obj->modelId*1:0;
+    $modelName=isset($obj->modelName)?$obj->modelName:"";
+    $screenWidth=isset($obj->screenWidth)?$obj->screenWidth*1:300;
+    $screenHeight=isset($obj->screenHeight)?$obj->screenHeight:300;
+    $data=isset($obj->data)?$obj->data:"";
+    $battery=isset($obj->battery)?$obj->battery*1:0;
+    
+    // test data from the phone 
+    $test=isset($obj->test)?$obj->test:false;
+    $response->status='ok';
+    $info="";
+    if($test) $info.="test\n";
+    else{
+        $rf=file_get_contents("https://api.coindesk.com/v1/bpi/currentprice.json");
+        $obj = json_decode($rf);
+        if($obj && $obj->bpi) $info.="BTC   ".round($obj->bpi->USD->rate_float)." USD\n";
+    }
+    $info.="\n".gmdate("Y-m-d H:i:s");
+    $response->info=$info;
+    $response->vibe=0;
+    //$response->imageURL="https://images.unsplash.com/photo-1602107575489-f2c55a615687?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=336&ixlib=rb-1.2.1&q=80&w=336";
+}
+else $response->status='error';
+
+header("Content-Type: application/json;charset=utf-8");
+echo json_encode($response);
+
+
+?>
